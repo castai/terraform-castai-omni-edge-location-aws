770fb04d872b58035ea7034e565e62a60a7caae5
Claudio Lorina <claudio@cast.ai>
2026-03-05 15:37:22 +0100

    feat: add InstanceProfile field support (#9)

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     castai = {
       source  = "castai/castai"
-      version = ">= 8.12.0"
+      version = ">= 8.19.0"
     }
     aws = {
       source  = "hashicorp/aws"
