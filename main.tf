e5f75b615082b0e5f2b6b8f08645a1de8df5633a
Andrej Kislovskij <andrej@cast.ai>
2026-06-02 12:55:25 +0300

    Aligned with other location modules

--- main.tf
+++ main.tf
@@ -372,8 +372,8 @@ resource "castai_edge_configuration" "this" {
   cluster_id       = var.cluster_id
   edge_location_id = castai_edge_location.this.id
   name             = each.value.name
-  user_data_base64 = try(each.value.user_data_base64, null)
-  cri              = try(each.value.cri, {})
+  user_data_base64 = each.value.user_data_base64
+  cri              = each.value.cri
 
   aws = {
     image_id           = try(each.value.image_id, null)
@@ -389,12 +389,5 @@ resource "castai_edge_configuration_default" "this" {
   cluster_id       = var.cluster_id
   edge_location_id = castai_edge_location.this.id
   configuration_id = castai_edge_configuration.this[var.default_edge_configuration_name].id
-
-  lifecycle {
-    precondition {
-      condition     = var.default_edge_configuration_name == "" || can(castai_edge_configuration.this[var.default_edge_configuration_name])
-      error_message = "The specified default_edge_configuration_name '${var.default_edge_configuration_name}' does not match any key in var.edge_configurations."
-    }
-  }
 }
 
